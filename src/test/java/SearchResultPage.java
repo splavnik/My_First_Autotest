cca9e51d81a6ab7acd7d8448973bf3233fcafea1
Егор <100774035+splavnik@users.noreply.github.com>
2022-04-21 17:34:22 +0300

    Update SearchResultPage.java

--- src/test/java/SearchResultPage.java
+++ src/test/java/SearchResultPage.java
@@ -3,7 +3,10 @@ import com.codeborne.selenide.ElementsCollection;
 import static com.codeborne.selenide.Selenide.$$x;
 
 public class SearchResultPage {
-private final ElementsCollection SearchResult = $$x("//link[@rel='prerender']");
+    /**
+     * Создаем коллекцию из всех ссылок на странице с результатами поиска
+     */
+private final ElementsCollection SearchResult = $$x("//a[@href][@data-ved][@ping]");
 
     /**
      * Возвращает href из первого результата поиска
